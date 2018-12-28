fddf4d8b2817dc8de5d424547bf8fdb167f30d6e
bobanko <mishko.vladimir@gmail.com>
2018-12-28 20:18:23 +0200

    dictionary: letters emo set from heb

--- dictionaries/letters.js
+++ dictionaries/letters.js
@@ -1,156 +1,187 @@
 const LETTERS = [
   {
     heb: "א",
+    emo: "א",
     trxn: "а",
     rus: "алеф"
   },
   {
-    heb: "ב", //?
+    heb: "ב",
+    emo: "ב", //?
     trxn: "б",
     rus: "бет"
   },
   {
     heb: "ב",
+    emo: "ב",
     trxn: "в",
     rus: "вет"
   },
   {
     heb: "ג",
+    emo: "ג",
     trxn: "г",
     rus: "гимель"
   },
   {
     heb: "ד",
+    emo: "ד",
     trxn: "д",
     rus: "далет"
   },
   {
     heb: "ה",
+    emo: "ה",
     trxn: "(г/х/-)",
     rus: "гей"
   },
   {
     heb: "ו",
+    emo: "ו",
     trxn: "в",
     rus: "вав"
   },
   {
     heb: "וֹ",
+    emo: "וֹ",
     trxn: "о",
     rus: "вав"
   },
   {
     heb: "וּ",
+    emo: "וּ",
     trxn: "у",
     rus: "вав"
   },
   {
     heb: "ז",
+    emo: "ז",
     trxn: "з",
     rus: "зайн"
   },
   {
     heb: "ח",
+    emo: "ח",
     trxn: "х",
     rus: "хет"
   },
   {
     heb: "ט",
+    emo: "ט",
     trxn: "т",
     rus: "тет"
   },
   {
     heb: "י",
+    emo: "י",
     trxn: "и/й",
     rus: "йуд"
   },
   {
     heb: "כ",
+    emo: "כ",
     trxn: "к",
     rus: "каф"
   },
   {
-    heb: "ך", //??
+    heb: "ך",
+    emo: "ך", //??
     trxn: "х",
     rus: "хаф"
   },
   {
     heb: "ל",
+    emo: "ל",
     trxn: "л",
     rus: "ламед"
   },
   {
     heb: "מ",
+    emo: "מ",
     trxn: "м",
     rus: "мем"
   },
   {
     heb: "ם",
+    emo: "ם",
     trxn: "м.",
     rus: "мем софит"
   },
   {
     heb: "נ",
+    emo: "נ",
     trxn: "н",
     rus: "нун"
   },
   {
     heb: "ן",
+    emo: "ן",
     trxn: "н.",
     rus: "нун софит"
   },
   {
     heb: "ס",
+    emo: "ס",
     trxn: "с",
     rus: "самех"
   },
   {
     heb: "ע",
+    emo: "ע",
     trxn: "а",
     rus: "айн"
   },
   {
     heb: "פ",
+    emo: "פ",
     trxn: "п",
     rus: "пей"
   },
   {
     heb: "ף",
+    emo: "ף",
     trxn: "ф",
     rus: "фей"
   },
   {
     heb: "צ",
+    emo: "צ",
     trxn: "ц",
     rus: "цади"
   },
   {
     heb: "ץ",
+    emo: "ץ",
     trxn: "ц.",
     rus: "цади софит"
   },
   {
     heb: "ק",
+    emo: "ק",
     trxn: "к",
     rus: "куф"
   },
   {
     heb: "ר",
+    emo: "ר",
     trxn: "р",
     rus: "рейш"
   },
   {
     heb: "ש",
+    emo: "ש",
     trxn: "ш",
     rus: "шин"
   },
   {
     heb: "שׁ",
+    emo: "שׁ",
     trxn: "с",
     rus: "син"
   },
   {
     heb: "ת",
+    emo: "ת",
     trxn: "т",
     rus: "тав"
   }
